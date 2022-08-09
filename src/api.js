3041a35af772bd6f0b5c6abe06954e09210934aa
BINIT-Nayak <legendnayak48@gmail.com>
2022-08-10 00:06:51 +0530

    Adding file to repository

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ae68c7722amsh1e2a3b9d48389cdp18a9c1jsn124ed7937ad8',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL='https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY="b7dcc493d69b2e070d6c7f44ba802f7a"
